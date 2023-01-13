cce839e0bf036131f4b20f363b813a495e0f5081
rob <roobert@gmail.com>
2023-01-13 22:49:16 +0000

    Add lvim defaults (#1)

--- lua/tailwindcss-colorizer-cmp/init.lua
+++ lua/tailwindcss-colorizer-cmp/init.lua
@@ -1,6 +1,79 @@
 M = {}
 
 M.formatter = function(entry, vim_item)
+  -- FIXME:
+  -- * support lunarvim default cmp config until we can work out how to chain
+  --   formatters..
+  if lvim then
+    local max_width = 0
+
+    if max_width ~= 0 and #vim_item.abbr > max_width then
+      vim_item.abbr = string.sub(vim_item.abbr, 1, max_width - 1) .. lvim.icons.ui.Ellipsis
+    end
+
+    if lvim.use_icons then
+      vim_item.kind = lvim.icons.kind[vim_item.kind]
+
+      if entry.source.name == "copilot" then
+        vim_item.kind = lvim.icons.git.Octoface
+        vim_item.kind_hl_group = "CmpItemKindCopilot"
+      end
+
+      if entry.source.name == "cmp_tabnine" then
+        vim_item.kind = lvim.icons.misc.Robot
+        vim_item.kind_hl_group = "CmpItemKindTabnine"
+      end
+
+      if entry.source.name == "crates" then
+        vim_item.kind = lvim.icons.misc.Package
+        vim_item.kind_hl_group = "CmpItemKindCrate"
+      end
+
+      if entry.source.name == "lab.quick_data" then
+        vim_item.kind = lvim.icons.misc.CircuitBoard
+        vim_item.kind_hl_group = "CmpItemKindConstant"
+      end
+
+      if entry.source.name == "emoji" then
+        vim_item.kind = lvim.icons.misc.Smiley
+        vim_item.kind_hl_group = "CmpItemKindEmoji"
+      end
+    end
+
+    -- FIXME:
+    -- * work out how to access this stuff rather than making local copies..
+    -- vim_item.menu = lvim.builtin.cmp.formatting.source_names[entry.source.name]
+    -- vim_item.dup = lvim.builtin.cmp.formatting.duplicates[entry.source.name]
+    --     or lvim.builtin.cmp.formatting.duplicates_default
+    local source_names = {
+      nvim_lsp = "(LSP)",
+      emoji = "(Emoji)",
+      path = "(Path)",
+      calc = "(Calc)",
+      cmp_tabnine = "(Tabnine)",
+      vsnip = "(Snippet)",
+      luasnip = "(Snippet)",
+      buffer = "(Buffer)",
+      tmux = "(TMUX)",
+      copilot = "(Copilot)",
+      treesitter = "(TreeSitter)",
+    }
+
+    vim_item.menu = source_names[entry.source.name]
+
+    local duplicates = {
+      buffer = 1,
+      path = 1,
+      nvim_lsp = 0,
+      luasnip = 1,
+    }
+
+    local duplicates_default = 0
+
+    vim_item.dup = duplicates[entry.source.name] or duplicates_default
+
+  end
+
   if vim.tbl_contains({ 'nvim_lsp' }, entry.source.name) then
     local words = {}
     for word in string.gmatch(vim_item.word, "[^-]+") do
