8d6c299e4c4e001354b60f6a00a62aeedf514157
Rob Wilson <roobert@gmail.com>
2023-01-13 20:34:47 +0000

    Refactor

--- lua/tailwindcss-colorizer-cmp/init.lua
+++ lua/tailwindcss-colorizer-cmp/init.lua
@@ -1,51 +1,85 @@
 M = {}
 
 M.formatter = function(entry, vim_item)
-  if vim.tbl_contains({ 'nvim_lsp' }, entry.source.name) then
-    if vim_item.kind ~= "Color" then
-      return vim_item
+  if lvim then
+    local max_width = lvim.builtin.cmp.formatting.max_width
+
+    if max_width ~= 0 and #vim_item.abbr > max_width then
+      vim_item.abbr = string.sub(vim_item.abbr, 1, max_width - 1) .. lvim.icons.ui.Ellipsis
     end
 
-    -- FIXME:
-    -- starts with text- bg- from- fill-
-    -- if vim_item.word  "Color" then
-    --   return vim_item
-    -- end
+    if lvim.use_icons then
+      vim_item.kind = lvim.builtin.cmp.formatting.kind_icons[vim_item.kind]
 
+      if entry.source.name == "copilot" then
+        vim_item.kind = lvim.icons.git.Octoface
+        vim_item.kind_hl_group = "CmpItemKindCopilot"
+      end
+
+      if entry.source.name == "cmp_tabnine" then
+        vim_item.kind = lvim.icons.misc.Robot
+        vim_item.kind_hl_group = "CmpItemKindTabnine"
+      end
+
+      if entry.source.name == "crates" then
+        vim_item.kind = lvim.icons.misc.Package
+        vim_item.kind_hl_group = "CmpItemKindCrate"
+      end
+
+      if entry.source.name == "lab.quick_data" then
+        vim_item.kind = lvim.icons.misc.CircuitBoard
+        vim_item.kind_hl_group = "CmpItemKindConstant"
+      end
+
+      if entry.source.name == "emoji" then
+        vim_item.kind = lvim.icons.misc.Smiley
+        vim_item.kind_hl_group = "CmpItemKindEmoji"
+      end
+    end
+  end
+
+  -- vim_item.menu = lvim.builtin.cmp.formatting.source_names[entry.source.name]
+  -- vim_item.dup = lvim.builtin.cmp.formatting.duplicates[entry.source.name]
+  --     or lvim.builtin.cmp.formatting.duplicates_default
+
+  if vim.tbl_contains({ 'nvim_lsp' }, entry.source.name) then
     local words = {}
     for word in string.gmatch(vim_item.word, "[^-]+") do
       table.insert(words, word)
     end
 
-    -- FIXME: for xyz-x/y..
-    local color_name = words[2]
-    local color_number = words[3]
+    if #words > 3 or #words > 4 then
+      return vim_item
+    end
+
+    local color_name, color_number
+    if words[2] == "x" or words[2] == "y" then
+      color_name = words[3]
+      color_number = words[4]
+    else
+      color_name = words[2]
+      color_number = words[3]
+    end
 
     if not color_name or not color_number then
       return vim_item
     end
 
     local color_index = tonumber(color_number)
-
     local tailwindcss_colors = require("tailwindcss-colorizer-cmp.colors").TailwindcssColors
 
     if not tailwindcss_colors[color_name] then
       return vim_item
     end
 
-    -- FIXME:
-    local color_table = tailwindcss_colors[color_name]
-
-    if not color_table[color_index] then
+    if not tailwindcss_colors[color_name][color_index] then
       return vim_item
     end
 
-    local color = color_table[color_index]
+    local color = tailwindcss_colors[color_name][color_index]
 
     local hl_group = "lsp_documentColor_mf_" .. color
-
     vim.api.nvim_set_hl(0, hl_group, { fg = "#" .. color, bg = "#" .. color })
-
     vim_item.kind_hl_group = hl_group
 
     return vim_item
